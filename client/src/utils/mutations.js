6584ed1665aaab16d6e19e391c43ce4dd7999d01
Marek Polak <markalexpolak@gmail.com>
2023-10-02 18:27:41 -0400

    updated queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,62 @@ mutation addUser($userInput: UserInput!) {
       }
     }
   }`;
+
+  export const UPDATE_USER = gql `
+  mutation UpdateUser($userInput: UpdateUserInput!) {
+    updateUser(userInput: $userInput) {
+      _id
+      username
+      email
+    }
+  }`;
+  export const ADD_APPOINTMENT = gql `
+  mutation {
+    addAppointment(appointmentInput: {
+      name,
+      address,
+      phone,
+      email,
+      description,
+    }) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }`;
+  export const UPDATE_APPOINTMENT = gql `
+  mutation {
+    updateAppointment(appointmentInput: {
+      appointmentId,
+      name,
+      address,
+      phone,
+      email,
+      description,
+    }) {
+      _id
+      name
+      address
+      phone
+      email
+      website
+      description
+    }
+  }`;
+  export const DELETE_USER = gql `
+  mutation {
+    deleteAppointment(appointmentInput: {
+      appointmentId: "your_appointment_id_here"
+    }) {
+      _id
+      name
+      address
+      phone
+      email
+      website
+      description
+    }
+  }`;
