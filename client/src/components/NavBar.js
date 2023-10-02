88885da08a25944a6ef2fcfdb6829323e017f69f
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-02 19:18:48 -0400

    test

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,23 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return ( 
-        <div class="">
-            <div class="container p-2"> 
-                <div class="row">
-                    <div class="col-8"> 
-                        <NavLink to="/">Home</NavLink> 
-                        <span> - </span>
-                        <NavLink to="/about" >About</NavLink> 
-                    </div>
-                    <div class="col-2"></div>
-                    <div class="col-2 text-center"> 
-                        <NavLink to="/login" >Login</NavLink>
-                        <span> - </span>
-                        <NavLink to="/sign-up" >Sign-up</NavLink> 
-                    </div>
+        <div class="container p-2"> 
+            <div class="row">
+                <div class="col-8"> 
+                    <NavLink to="/">Home</NavLink> 
+                    <span> - </span>
+                    <NavLink to="/about" >About</NavLink> 
+                </div>
+                <div class="col-2"></div>
+                <div class="col-2 text-center"> 
+                    <NavLink to="/login" >Login</NavLink>
+                    <span> - </span>
+                    <NavLink to="/sign-up" >Sign-up</NavLink> 
                 </div>
             </div>
-        </div> 
+        </div>
     );
 }
 
