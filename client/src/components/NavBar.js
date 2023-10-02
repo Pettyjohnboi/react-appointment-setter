20c9cf30d1ed67b5ed5bc6b1ce3a300455fdd6fa
Jordan Pettyjohn <pettyjohn@gmail.com>
2023-10-02 18:32:15 -0400

    Updates

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,16 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return ( 
-        <div class="container p-4"> 
-            <div class="row">
-                <div class="col-8"> 
-                    <NavLink to="/" >  Home  </NavLink> 
-                    <NavLink to="/about" > About </NavLink> 
-                </div>
-                <div class="col-md-auto"></div>
-                <div class="col-3"> 
-                    <NavLink to="/login" >  Login  </NavLink>
-                    <NavLink to="/sign-up" > Sign-up </NavLink> 
+        <div class="">
+            <div class="container p-2"> 
+                <div class="row">
+                    <div class="col-8"> 
+                        <NavLink to="/">Home</NavLink> 
+                        <span> - </span>
+                        <NavLink to="/about" >About</NavLink> 
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-2 text-center"> 
+                        <NavLink to="/login" >Login</NavLink>
+                        <span> - </span>
+                        <NavLink to="/sign-up" >Sign-up</NavLink> 
+                    </div>
                 </div>
             </div>
         </div> 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,19 @@
 function Login(){
-    
+    return(
+        <div class="container">
+            <div class="row"> 
+                <div class="col">
+                <form>
+                    <input type="text" name="name" placeholder="Name"/>
+                    <br />
+                    <input type="email" name="email" placeholder="Email"/>
+                    <br />
+                    <input type="submit" value="Submit"/>
+                </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
