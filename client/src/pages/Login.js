1590e8d0c7ee21e32cb4dc965281fc0974efbb67
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-03 11:45:06 -0400

    blah

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,13 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { GET_ME } from "../utils/queries";
+import { useNavigate } from "react-router-dom"; 
+
+
 function Login(){
-    return(
-        <div class="container">
-            <div class="row"> 
-                <div class="col">
-                <form>
-                    <input type="text" name="name" placeholder="Name"/>
-                    <br />
-                    <input type="email" name="email" placeholder="Email"/>
-                    <br />
-                    <input type="submit" value="Submit"/>
-                </form>
-                </div>
-            </div>
-        </div>
-    )
+
 }
 
 export default Login;
