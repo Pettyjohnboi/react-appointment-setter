74c7dae535372454048072750b2405e6b73b1dde
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-04 19:49:51 -0400

    Starting of appointment page

--- client/src/pages/Appointments.js
+++ client/src/pages/Appointments.js
@@ -1,16 +1,20 @@
+import Auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_APPOINTMENTS } from "../utils/queries";
 
 function Appointments() {
 
-    const getAllAppointments = useQuery(GET_ALL_APPOINTMENTS);
-    console.log(getAllAppointments);
-
-    //let appArr = getAllAppointments.data.allAppointments;
-
+    const { data } = useQuery(
+        GET_ALL_APPOINTMENTS, 
+        {     variables: 
+            {       
+                userId: Auth.getProfile().data._id,     
+            },   
+        });
+    console.log(data.allAppointments);
     return (
         <div class="container p-5"> 
-
+        
         </div>
     );
 }
