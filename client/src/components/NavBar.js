130f97e9ef87a2a6367eaa11c37548908bdeae2c
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-04 15:34:37 -0400

    Logout button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,24 @@
 import React from 'react'; 
 import { NavLink } from 'react-router-dom';  
+import Auth from '../utils/auth';
 
 function NavBar() {
+ 
+    const LoginContext = Auth.loggedIn();
+    let section;
+
+    if (LoginContext) {
+        section =   <div class="col-2 text-center"> 
+                        <NavLink to="/" className="inactive" activeClassName="active" onClick={Auth.logout}>Logout</NavLink>
+                    </div>
+    } else {
+        section = <div class="col-2 text-center"> 
+            <NavLink to="/login" className="inactive" activeClassName="active">Login</NavLink>
+            <span class="text-light"> - </span>
+            <NavLink to="/sign-up" className="inactive" activeClassName="active">Sign-up</NavLink> 
+        </div>
+    }
+
     return ( 
         <div class="container p-2"> 
             <div class="row">
@@ -11,12 +28,8 @@ function NavBar() {
                     <NavLink to="/appointments " className="inactive" activeClassName="active">Appointments</NavLink> 
                 </div>
                 <div class="col-2"></div>
-                <div class="col-2 text-center"> 
-                    <NavLink to="/login" className="inactive" activeClassName="active">Login</NavLink>
-                    <span class="text-light"> - </span>
-                    <NavLink to="/sign-up" className="inactive" activeClassName="active">Sign-up</NavLink> 
+                    {section}
                 </div>
-            </div>
         </div>
     );
 }
